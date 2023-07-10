737cdd85de9d204d7514ceb2f9b8f287b99b5918
Dawn Zac <43729160+Dawnzac@users.noreply.github.com>
2023-07-10 23:23:11 +0530

    Update batchcpy.bat

--- batchcpy.bat
+++ batchcpy.bat
@@ -2,11 +2,11 @@
 
 CHCP 65001 > NUL
 
-FOR /F "usebackq delims="  %%I IN ("D:\code projects\pytrain\C\list.txt") DO (
+FOR /F "usebackq delims="  %%I IN ("{path to }\list.txt") DO (
     xcopy /s "D:\Downloads\Documents\%%I" "C:\Windows\System32\%%I*"
 )
 
-FOR /F "usebackq delims="  %%A IN ("D:\code projects\pytrain\C\list.txt") DO (
+FOR /F "usebackq delims="  %%A IN ("{path to }\list.txt") DO (
     xcopy /s "D:\Downloads\Documents\%%A" "C:\Windows\SysWOW64\%%A*"
 )
 

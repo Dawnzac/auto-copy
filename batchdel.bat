e074a81173f4917d7dc6868dbbae25bb70527f88
Dawn Zac <43729160+Dawnzac@users.noreply.github.com>
2023-07-10 23:22:54 +0530

    Update batchdel.bat

--- batchdel.bat
+++ batchdel.bat
@@ -2,11 +2,11 @@
 
 CHCP 65001 > NUL
 
-FOR /F "usebackq delims="  %%I IN ("D:\code projects\pytrain\C\list.txt") DO (
+FOR /F "usebackq delims="  %%I IN ("{path to }\list.txt") DO (
     del /s "C:\Windows\System32\%%I*"
 )
 
-FOR /F "usebackq delims="  %%I IN ("D:\code projects\pytrain\C\list.txt") DO (
+FOR /F "usebackq delims="  %%I IN ("{path to }\list.txt") DO (
     del /s "C:\Windows\SysWOW64\%%I*"
 )
 

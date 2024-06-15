9759775a113d6600819df97171f7f8097b3d57d4
Dawn Zac <43729160+Dawnzac@users.noreply.github.com>
2024-06-15 22:29:52 +0530

    Update batchcpy.bat

--- batchcpy.bat
+++ batchcpy.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 CHCP 65001 > NUL
-
+REM start for loop from list
 FOR /F "usebackq delims="  %%I IN ("{path to }\list.txt") DO (
     xcopy /s "D:\Downloads\Documents\%%I" "C:\Windows\System32\%%I*"
 )
